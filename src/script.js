2ab917b8c3791f72bd2be49c36002fc831a448a9
Joefrey Mahusay <joefreymahusay@Joefreys-MacBook-Pro.local>
2023-10-12 11:23:36 +0800

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,57 @@
+import Swiper from "swiper";
+var $ = require("jquery");
+
+import "swiper/swiper-bundle.css";
+import { Navigation, Pagination } from "swiper/modules";
+$(function () {
+  var mobileMenuToggle = function () {
+    $(".js-menu-toggle").on("click", function (e) {
+      e.preventDefault();
+      if ($("body").hasClass("mobile-menu-active")) {
+        $("body").removeClass("mobile-menu-active");
+      } else {
+        $("body").addClass("mobile-menu-active");
+      }
+    });
+
+    $(window)
+      .on("resize", function () {
+        var $this = $(this);
+        if ($this.width() > 768) {
+          if ($("body").hasClass("mobile-menu-active")) {
+            $("body").removeClass("mobile-menu-active");
+          }
+        }
+      })
+      .on("resize");
+
+    $("body").on("click", function () {
+      $(this).removeClass("mobile-menu-active");
+    });
+
+    $(".js-mobile-close").on("click", function (e) {
+      e.preventDefault();
+      $("body").removeClass("mobile-menu-active");
+    });
+
+    $(".mobile-menu, .js-menu-toggle").on("click", function (event) {
+      event.stopPropagation();
+    });
+  };
+  mobileMenuToggle();
+  const swiper = new Swiper(".testimonialSwiper", {
+    loop: true,
+    slidesPerView: 1,
+    speed: 700,
+    spaceBetween: 20,
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Arial", "sans-serif", "system-ui", "ui-sans-serif"],
+        serif: ["Playfair Display", "Georgia", "ui-serif", "serif"],
+      },
+      colors: {
+        primary: "#003d35",
+        secondary: "#d2de32",
+        dark: "#1b1e21",
+        gray: "#3f4242",
+      },
+    },
+  },
+  plugins: [],
+};
